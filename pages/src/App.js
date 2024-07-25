f0b8bf014c3e475315c3ca1f9e683ef6e94c8368
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2024-07-25 13:57:12 +0530

    building the navbar and footer

--- pages/src/App.js
+++ pages/src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeComponent from './components/home-component/home-component.js';
 import './App.css';
+import './colors.css'
 
 function App() {
   return (
     <Router>
-      <div>
-        <header>
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-            </ul>
-          </nav>
-        </header>
-
-        <Routes>
-          <Route path="/" exact component={HomeComponent} />
-        </Routes>
-
-        <footer>
-          <p>&copy; 2024 Rachit Dhar. All rights reserved.</p>
-          <p>Contact at: <a href="mailto:rachitdhar2511@gmail.com">rachitdhar2511@gmail.com</a></p>
-        </footer>
-      </div>
+      <Routes>
+        <Route path="/" exact element={<HomeComponent/>} />
+      </Routes>
     </Router>
   );
 }
--- pages/src/components/home-component/home-component.js
+++ pages/src/components/home-component/home-component.js
@@ -1,7 +1,23 @@
-function HomeComponent() {
+import React from "react";
+import { Link } from 'react-router-dom';
+import './home-component.css'
+
+export default function HomeComponent() {
     return (
-        <a>Hello world!</a>
+        <div>
+            <nav class="navbar">
+                <ul class="navbar-menu">
+                    <li class="navbar-item"><Link class="link" to="/">Home</Link></li>
+                    <li class="navbar-item"><Link class="link" to="/posts">Posts</Link></li>
+                </ul>
+            </nav>
+            <div class="content">
+                <a>Hello world</a>
+            </div>
+            <div class="footer">
+                <p>&copy; 2024 Rachit Dhar. All rights reserved.</p>
+                <p>Contact at: <a href="mailto:rachitdhar2511@gmail.com">rachitdhar2511@gmail.com</a></p>
+            </div>
+        </div>
     );
 }
-
-export default HomeComponent;
