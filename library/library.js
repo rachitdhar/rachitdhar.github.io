7f87c71f127d8f71f630f381ecfd34a1b23cc3cc
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2025-07-12 14:26:34 +0530

    add fetch and ui handling for books and posts list

--- library/library.js
+++ library/library.js
@@ -1,30 +1,56 @@
-// sample data (in reality, this will be obtained from an api)
-const data = {
-    fiction: [
-    ],
-    nonfiction: [
-    ],
-    academic: [
-    ]
+const API_URL = "http://localhost:8000/api";
+const tbody_fic = document.getElementById('fiction-body');
+const tbody_nfic = document.getElementById('nonfiction-body');
+const tbody_acad = document.getElementById('academic-body');
+
+const book_status = {
+    NR: 'NR',
+    CR: 'CR',
+    RD: 'RD'
+};
+const book_classification = {
+    FIC: 'FIC',
+    NFIC: 'NFIC',
+    ACAD: 'ACAD'
 };
 
-function populateTable(sectionId, books) {
-    const tbody = document.getElementById(sectionId);
-    books.forEach(entry => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `
-        <td>${entry.book}</td>
-        <td>${entry.author}</td>
-        <td>${entry.status}</td>
-    `;
-    tbody.appendChild(tr);
+
+function populateTables(books) {
+    books.forEach(row => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${row.name}</td>
+            <td>${row.author_name}</td>
+            <td>${row.status}</td>
+        `;
+
+        switch (row.classification_code) {
+        case book_classification.FIC:
+            tbody_fic.appendChild(tr);
+            break;
+        case book_classification.NFIC:
+            tbody_nfic.appendChild(tr);
+            break;
+        case book_classification.ACAD:
+            tbody_acad.appendChild(tr);
+            break;
+        }
     });
 }
 
-populateTable("fiction-body", data.fiction);
-populateTable("nonfiction-body", data.nonfiction);
-populateTable("academic-body", data.academic);
-
 function handleStatusInfoClick() {
-    alert('Not Read / Reading / Read');
+    alert('Not Read / Currently Reading / Read');
 }
+
+// entry point
+fetch(`${API_URL}/books`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(res => populateTables(res.data))
+  .catch(error => {
+    console.error('GET Error: ', error);
+  });
