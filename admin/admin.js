31465a63ffdda65ba74bac38894f6556740ac929
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2025-07-29 12:49:42 +0530

    created auth page

---
+++ admin/admin.js
@@ -0,0 +1,35 @@
+const API_URL = "https://unified-jennet-mildly.ngrok-free.app/api";
+const loader = document.querySelector('.loader-overlay');
+
+function login() {
+    loader.style.display = 'flex';
+
+    const userid = document.getElementById("username").value;
+    const passw = document.getElementById("password").value;
+
+    fetch(`${API_URL}/auth`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'ngrok-skip-browser-warning': 'true'
+        },
+        body: JSON.stringify({
+            userid: userid,
+            password: passw
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Login failed');
+        }
+        return response.json();
+    })
+    .then(data => {
+        window.location.href('index.html');
+    })
+    .catch(error => {
+        console.error('POST Error: ', error);
+        loader.style.display = 'none';
+        toastr.error('Authentication failed', 'Error');
+    });
+}
